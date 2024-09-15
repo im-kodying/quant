17dbd5ee220c9284780837fcaad7a069836f2e3f
kody <63944072+im-kodying@users.noreply.github.com>
2024-09-15 10:18:46 -0400

    add docker ibkr-gateway step

---
+++ ibkr.py
@@ -0,0 +1,21 @@
+from nautilus_trader.adapters.interactive_brokers.config import DockerizedIBGatewayConfig
+from nautilus_trader.adapters.interactive_brokers.gateway import DockerizedIBGateway
+
+if __name__ == "main":
+    gateway_config = DockerizedIBGatewayConfig(
+        username="imkodying",
+        password="cvNQOH3qRAT3ze8F5yr4",
+        trading_mode="paper",
+    )
+
+    # This may take a short while to start up, especially the first time
+    gateway = DockerizedIBGateway(
+        config=gateway_config
+    )
+    gateway.start()
+
+    # Confirm you are logged in
+    print(gateway.is_logged_in(gateway.container))
+
+    # Inspect the logs
+    print(gateway.container.logs())
