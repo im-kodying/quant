e3c6c3b32853a59fdd20b7828a00cedc5821cc9c
kody <63944072+im-kodying@users.noreply.github.com>
2024-10-06 14:06:24 -0400

    add loggings & turn off live trading

--- main.py
+++ main.py
@@ -31,71 +31,73 @@ logger.setLevel("INFO")
 
 async def main_live():
     logger.info("Kody: Starting Quant Trading Platform . . .")
-    host = "127.0.0.1"
-    port = 4002
-    instrument_provider_config = InteractiveBrokersInstrumentProviderConfig(
-        build_futures_chain=False,  # Set to True if fetching futures
-        build_options_chain=False,  # Set to True if fetching options
-        min_expiry_days=10,  # Relevant for futures/options with expiration
-        max_expiry_days=60,  # Relevant for futures/options with expiration
-        load_ids=frozenset(
-            [
-                "EUR/USD.IDEALPRO",
-                "BTC/USD.PAXOS",
-                "SPY.ARCA",
-                "V.NYSE",
-                "YMH24.CBOT",
-                "CLZ27.NYMEX",
-                "ESZ27.CME",
-            ],
-        ),
-        load_contracts=frozenset(
-            [
-                IBContract(secType='STK', symbol='SPY', exchange='SMART', primaryExchange='ARCA'),
-                IBContract(secType='STK', symbol='AAPL', exchange='SMART', primaryExchange='NASDAQ')
-            ]
-        ),
-    )
-    data_client_config = InteractiveBrokersDataClientConfig(
-        ibg_port=port,
-        ibg_host=host,
-        handle_revised_bars=False,
-        use_regular_trading_hours=True,
-        market_data_type=IBMarketDataTypeEnum.DELAYED_FROZEN,  # Default is REALTIME if not set
-        instrument_provider=instrument_provider_config,
-    )
-    exec_client_config = InteractiveBrokersExecClientConfig(
-        ibg_port=port,
-        ibg_host=host,
-        account_id="DU123456",  # Must match the connected IB Gateway/TWS
-        instrument_provider=instrument_provider_config,
-        routing=RoutingConfig(
-            default=True,
-        )
-    )
-    try:
-        config_node = TradingNodeConfig(
-            trader_id="TESTER-001",
-            logging=LoggingConfig(log_level="INFO"),
-            data_clients={"IB": data_client_config},
-            exec_clients={"IB": exec_client_config},
-            data_engine=LiveDataEngineConfig(
-                time_bars_timestamp_on_close=False,  # Use opening time as `ts_event`, as per IB standard
-                validate_data_sequence=True,  # Discards bars received out of sequence
-            ),
-        )
-
-        node = TradingNode(config=config_node)
-        node.add_data_client_factory("IB", InteractiveBrokersLiveDataClientFactory)
-        node.add_exec_client_factory("IB", InteractiveBrokersLiveExecClientFactory)
-        node.build()
-        node.portfolio.set_specific_venue(IB_VENUE)
-        node.run()
-    finally:
-        node.dispose()
+    # host = "127.0.0.1"
+    # port = 4002
+    # instrument_provider_config = InteractiveBrokersInstrumentProviderConfig(
+    #     build_futures_chain=False,  # Set to True if fetching futures
+    #     build_options_chain=False,  # Set to True if fetching options
+    #     min_expiry_days=10,  # Relevant for futures/options with expiration
+    #     max_expiry_days=60,  # Relevant for futures/options with expiration
+    #     load_ids=frozenset(
+    #         [
+    #             "EUR/USD.IDEALPRO",
+    #             "BTC/USD.PAXOS",
+    #             "SPY.ARCA",
+    #             "V.NYSE",
+    #             "YMH24.CBOT",
+    #             "CLZ27.NYMEX",
+    #             "ESZ27.CME",
+    #         ],
+    #     ),
+    #     load_contracts=frozenset(
+    #         [
+    #             IBContract(secType='STK', symbol='SPY', exchange='SMART', primaryExchange='ARCA'),
+    #             IBContract(secType='STK', symbol='AAPL', exchange='SMART', primaryExchange='NASDAQ')
+    #         ]
+    #     ),
+    # )
+    # data_client_config = InteractiveBrokersDataClientConfig(
+    #     ibg_port=port,
+    #     ibg_host=host,
+    #     handle_revised_bars=False,
+    #     use_regular_trading_hours=True,
+    #     market_data_type=IBMarketDataTypeEnum.DELAYED_FROZEN,  # Default is REALTIME if not set
+    #     instrument_provider=instrument_provider_config,
+    # )
+    # exec_client_config = InteractiveBrokersExecClientConfig(
+    #     ibg_port=port,
+    #     ibg_host=host,
+    #     account_id="DU123456",  # Must match the connected IB Gateway/TWS
+    #     instrument_provider=instrument_provider_config,
+    #     routing=RoutingConfig(
+    #         default=True,
+    #     )
+    # )
+    # try:
+    #     config_node = TradingNodeConfig(
+    #         trader_id="TESTER-001",
+    #         logging=LoggingConfig(log_level="INFO"),
+    #         data_clients={"IB": data_client_config},
+    #         exec_clients={"IB": exec_client_config},
+    #         data_engine=LiveDataEngineConfig(
+    #             time_bars_timestamp_on_close=False,  # Use opening time as `ts_event`, as per IB standard
+    #             validate_data_sequence=True,  # Discards bars received out of sequence
+    #         ),
+    #     )
+    #
+    #     node = TradingNode(config=config_node)
+    #     node.add_data_client_factory("IB", InteractiveBrokersLiveDataClientFactory)
+    #     node.add_exec_client_factory("IB", InteractiveBrokersLiveExecClientFactory)
+    #     node.build()
+    #     node.portfolio.set_specific_venue(IB_VENUE)
+    #     node.run()
+    # finally:
+    #     node.dispose()
     return
 
+
 async def main_backtest():
+    logger.warning("Kody: Backtest Started")
     host = "127.0.0.1"
     port = 4002
     contract = IBContract(
@@ -109,7 +111,7 @@ async def main_backtest():
     client = HistoricInteractiveBrokersClient(host=host, port=port, client_id=5)
     await client.connect()
     await asyncio.sleep(2)
-
+    logger.warning("Kody: Client Connected")
     start = datetime.datetime(2023, 11, 6, 10, 0)
     end = datetime.datetime(2023, 11, 6, 16, 30)
 
@@ -196,10 +198,13 @@ async def main_backtest():
 
     results = node.run()
     logger.info("Kody", results)
+    return
 
 
 async def main():
     await asyncio.gather(main_live(), main_backtest())
+    return
+
 
 if __name__ == "__main__":
     asyncio.run(main())
